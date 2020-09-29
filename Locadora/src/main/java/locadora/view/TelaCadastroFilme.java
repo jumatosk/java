7bb3a1adece7441ff2be2320c5b7f88707452dca
Jumts <julianamattos201313@gmail.com>
2020-09-29 15:19:48 -0300

    Added CadastroFilme screen and removed images

--- Locadora/src/main/java/locadora/view/TelaCadastroFilme.java
+++ Locadora/src/main/java/locadora/view/TelaCadastroFilme.java
@@ -27,17 +27,157 @@ public class TelaCadastroFilme extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanelCadastroFilme = new javax.swing.JPanel();
+        jLabelTitulo = new javax.swing.JLabel();
+        jLabelGenero = new javax.swing.JLabel();
+        jLabelSinopse = new javax.swing.JLabel();
+        jLabelDuracao = new javax.swing.JLabel();
+        jButtonSalvar = new javax.swing.JButton();
+        jButtonLimpar = new javax.swing.JButton();
+        jButtonCancelar = new javax.swing.JButton();
+        jButtonConsultar = new javax.swing.JButton();
+        jTextFieldTitulo = new javax.swing.JTextField();
+        jComboBoxGenero = new javax.swing.JComboBox<>();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextAreaSinopse = new javax.swing.JTextArea();
+        jSpinnerDuracao = new javax.swing.JSpinner();
+        jLabelCadastroFilme = new javax.swing.JLabel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Vídeo Locadora Hora da Pipoca ");
+
+        jPanelCadastroFilme.setBackground(new java.awt.Color(102, 102, 102));
+
+        jLabelTitulo.setFont(new java.awt.Font("Calibri", 0, 18)); // NOI18N
+        jLabelTitulo.setForeground(new java.awt.Color(255, 255, 255));
+        jLabelTitulo.setText("Título:");
+
+        jLabelGenero.setFont(new java.awt.Font("Calibri", 0, 18)); // NOI18N
+        jLabelGenero.setForeground(new java.awt.Color(255, 255, 255));
+        jLabelGenero.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabelGenero.setText("Gênero:");
+
+        jLabelSinopse.setFont(new java.awt.Font("Calibri", 0, 18)); // NOI18N
+        jLabelSinopse.setForeground(new java.awt.Color(255, 255, 255));
+        jLabelSinopse.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabelSinopse.setText("Sinopse:");
+        jLabelSinopse.setToolTipText("");
+
+        jLabelDuracao.setFont(new java.awt.Font("Calibri", 0, 18)); // NOI18N
+        jLabelDuracao.setForeground(new java.awt.Color(255, 255, 255));
+        jLabelDuracao.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabelDuracao.setText("Duração:");
+
+        jButtonSalvar.setFont(new java.awt.Font("Calibri", 0, 18)); // NOI18N
+        jButtonSalvar.setText("Salvar");
+
+        jButtonLimpar.setFont(new java.awt.Font("Calibri", 0, 18)); // NOI18N
+        jButtonLimpar.setText("Limpar");
+
+        jButtonCancelar.setFont(new java.awt.Font("Calibri", 0, 18)); // NOI18N
+        jButtonCancelar.setText("Cancelar");
+
+        jButtonConsultar.setFont(new java.awt.Font("Calibri", 0, 18)); // NOI18N
+        jButtonConsultar.setText("Consultar");
+        jButtonConsultar.setToolTipText("");
+
+        jTextFieldTitulo.setFont(new java.awt.Font("Calibri", 0, 12)); // NOI18N
+
+        jComboBoxGenero.setEditable(true);
+        jComboBoxGenero.setFont(new java.awt.Font("Calibri", 0, 14)); // NOI18N
+        jComboBoxGenero.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Selecione um gênero", "Ação", "Animação", "Aventura", "Cinema de arte", "Chanchada", "Comédia", "Comédia romântica", "Comédia dramática", "Comédia de ação", "Dança", "Documentário", "Docuficção", "Drama", "Espionagem", "Faroeste", "Fantasia científica", "Ficção científica", "Filmes de guerra", "Musical", "Filme policial", "Romance", "Seriado", "Suspense", "Terror" }));
+
+        jTextAreaSinopse.setColumns(20);
+        jTextAreaSinopse.setFont(new java.awt.Font("Calibri", 0, 12)); // NOI18N
+        jTextAreaSinopse.setRows(5);
+        jScrollPane1.setViewportView(jTextAreaSinopse);
+
+        jSpinnerDuracao.setFont(new java.awt.Font("Calibri", 0, 14)); // NOI18N
+        jSpinnerDuracao.setModel(new javax.swing.SpinnerNumberModel(20, 20, 360, 10));
+
+        javax.swing.GroupLayout jPanelCadastroFilmeLayout = new javax.swing.GroupLayout(jPanelCadastroFilme);
+        jPanelCadastroFilme.setLayout(jPanelCadastroFilmeLayout);
+        jPanelCadastroFilmeLayout.setHorizontalGroup(
+            jPanelCadastroFilmeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanelCadastroFilmeLayout.createSequentialGroup()
+                .addGroup(jPanelCadastroFilmeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanelCadastroFilmeLayout.createSequentialGroup()
+                        .addGap(68, 68, 68)
+                        .addGroup(jPanelCadastroFilmeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jLabelDuracao)
+                            .addComponent(jLabelSinopse)
+                            .addComponent(jLabelGenero)
+                            .addComponent(jLabelTitulo))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(jPanelCadastroFilmeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jTextFieldTitulo)
+                            .addComponent(jComboBoxGenero, javax.swing.GroupLayout.PREFERRED_SIZE, 175, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 572, Short.MAX_VALUE)
+                            .addComponent(jSpinnerDuracao, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(jPanelCadastroFilmeLayout.createSequentialGroup()
+                        .addGap(135, 135, 135)
+                        .addComponent(jButtonSalvar)
+                        .addGap(52, 52, 52)
+                        .addComponent(jButtonLimpar)
+                        .addGap(48, 48, 48)
+                        .addComponent(jButtonCancelar)
+                        .addGap(62, 62, 62)
+                        .addComponent(jButtonConsultar)))
+                .addContainerGap(63, Short.MAX_VALUE))
+        );
+        jPanelCadastroFilmeLayout.setVerticalGroup(
+            jPanelCadastroFilmeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanelCadastroFilmeLayout.createSequentialGroup()
+                .addGap(62, 62, 62)
+                .addGroup(jPanelCadastroFilmeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabelTitulo)
+                    .addComponent(jTextFieldTitulo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(34, 34, 34)
+                .addGroup(jPanelCadastroFilmeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabelGenero)
+                    .addComponent(jComboBoxGenero, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(38, 38, 38)
+                .addGroup(jPanelCadastroFilmeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabelSinopse))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 33, Short.MAX_VALUE)
+                .addGroup(jPanelCadastroFilmeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabelDuracao)
+                    .addComponent(jSpinnerDuracao, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(53, 53, 53)
+                .addGroup(jPanelCadastroFilmeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButtonSalvar)
+                    .addComponent(jButtonLimpar)
+                    .addComponent(jButtonCancelar)
+                    .addComponent(jButtonConsultar))
+                .addGap(55, 55, 55))
+        );
+
+        jLabelCadastroFilme.setFont(new java.awt.Font("Calibri", 1, 24)); // NOI18N
+        jLabelCadastroFilme.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabelCadastroFilme.setText("Cadastro de Filme");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanelCadastroFilme, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabelCadastroFilme)
+                .addGap(299, 299, 299))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(26, 26, 26)
+                .addComponent(jLabelCadastroFilme)
+                .addGap(43, 43, 43)
+                .addComponent(jPanelCadastroFilme, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
 
         pack();
@@ -79,5 +219,20 @@ public class TelaCadastroFilme extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButtonCancelar;
+    private javax.swing.JButton jButtonConsultar;
+    private javax.swing.JButton jButtonLimpar;
+    private javax.swing.JButton jButtonSalvar;
+    private javax.swing.JComboBox<String> jComboBoxGenero;
+    private javax.swing.JLabel jLabelCadastroFilme;
+    private javax.swing.JLabel jLabelDuracao;
+    private javax.swing.JLabel jLabelGenero;
+    private javax.swing.JLabel jLabelSinopse;
+    private javax.swing.JLabel jLabelTitulo;
+    private javax.swing.JPanel jPanelCadastroFilme;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JSpinner jSpinnerDuracao;
+    private javax.swing.JTextArea jTextAreaSinopse;
+    private javax.swing.JTextField jTextFieldTitulo;
     // End of variables declaration//GEN-END:variables
 }
